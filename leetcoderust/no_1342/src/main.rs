12386dcf9edf7be8b0803a1e6b4c518f31b9f7d2
Xavier Jiang <zhqjiang@outlook.com>
2022-05-28 12:21:35 +0800

    fix: 1342

--- leetcoderust/no_1342/src/main.rs
+++ leetcoderust/no_1342/src/main.rs
@@ -10,16 +10,22 @@ impl Solution {
     //         }
     //     }
     // }
+    // pub fn number_of_steps(num: i32) -> i32 {
+    //     match (num, num % 2 == 1) {
+    //         (0, _) => 0,
+    //         (_, true) => 1 + Solution::number_of_steps(num - 1),
+    //         (_, false) => 1 + Solution::number_of_steps(num / 2),
+    //     }
+    // }
     pub fn number_of_steps(num: i32) -> i32 {
-        match (num, num % 2 == 1) {
-            (0, _) => 0,
-            (_, true) => 1 + Solution::number_of_steps(num - 1),
-            (_, false) => 1 + Solution::number_of_steps(num / 2),
-        }
+        num.count_ones() as i32 + (format!("{:b}", num).len()) as i32 - 1
     }
 }
 
 struct Solution {}
 fn main() {
-    assert_eq!(Solution::number_of_steps(4), 2);
+    // println!("{:b}", 4);
+    // println!("{:b}", 14);
+    assert_eq!(Solution::number_of_steps(4), 3);
+    assert_eq!(Solution::number_of_steps(14), 6);
 }
